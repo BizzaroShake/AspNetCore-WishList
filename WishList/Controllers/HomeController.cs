f78ccc280fa9b5c847504afffe1cdda3e2813799
Benjamin Kluge <Ben.Kluge@gmail.com>
2022-07-08 15:08:11 -0500

    Create Home Views and the HomeController

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController
+    {
+        public Task<IActionResult> Index()
+        {
+            return Index();
+        }
+
+        public Task<IActionResult> Error()
+        {
+            return Error();
+        }
+    }
+
+
+}
